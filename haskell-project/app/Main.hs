f365e9aacfb7acf1e461b337556ea702188bf685
Kacper Wojdyla <kacper.wojdyla@yahoo.com>
2022-12-20 13:13:32 +0000

    creating first user thread

--- haskell-project/app/Main.hs
+++ haskell-project/app/Main.hs
@@ -6,10 +6,18 @@ import Control.Concurrent
 import System.Random
 
 createUser :: Int -> User
-createUser n = User n username
-    where username = "User " ++ show n
+createUser n = User n ("User" ++ show n)
+
+sendMsg :: String -> String -> IO ()
+sendMsg user msg = do
+    let userText = (user ++ " || ")
+    putStrLn (userText ++ msg)
 
 main :: IO ()
 main = do
     let users = map createUser [1..10]
-    print (head users)
+    let msgs = ["hello", "cześć", "hola", "bonjour", "guten tag", "salve", "nǐn hǎo", "olá", "asalaam alaikum", "konnichiwa", "anyoung haseyo", "zdravstvuyte"]
+    let user1 = username (head users)
+    let msg1 = head msgs
+    threadID <- forkIO (sendMsg user1 msg1)
+    putStrLn "FINISH"
